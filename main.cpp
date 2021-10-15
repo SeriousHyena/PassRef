b5769d894d8a31b4540990b1005e0e5ad7d1572b
SeriousHyena <emackiii@gmail.com>
2021-10-15 07:59:30 -0700

    progress

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+//declare functions
+void passRef(float input, float& output);
+
+//declare variables here
+float myNum = 45.0;
+
+int main()
+{
+	void passRef(float input, float& output);			//invoke the function
+
+	cout << "myNum = :"<<myNum<< endl;
+	passRef(myNum, myNum);
+	cout <<"The input divided by 6 is: " << myNum;
+	cout << endl << endl;
+	return 0;
+}
+
+void passRef(float input, float& output)
+{
+	output = input / 6;
+}
